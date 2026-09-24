c9d0a2bffda97422af1a2b4059764028fadf51f3
Wilian Mostaço <mostacowilian28@gmail.com>
2026-09-24 11:02:06 -0300

    Set up Sequelize with SQLite database configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,17 @@
+const { Sequelize } = require("sequelize");
+const path = require("path");
 
+const databasePath = path.join(
+    __dirname,
+    "..",
+    "data",
+    "database.sqlite"
+);
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: databasePath,
+    logging: false
+});
+
+module.exports = sequelize;
